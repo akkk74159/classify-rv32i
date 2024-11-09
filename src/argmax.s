0548759279998ffb87bf09ac258c7b6bfab0dfbb
Eric Ruan <p76121071@gs.ncku.edu.tw>
2024-11-09 16:26:00 +0800

    Finish argmax.s

--- src/argmax.s
+++ src/argmax.s
@@ -25,12 +25,30 @@ argmax:
     li t6, 1
     blt a1, t6, handle_error
 
-    lw t0, 0(a0)
+    lw t0, 0(a0)							# t0 = max element
 
-    li t1, 0
-    li t2, 1
+    li t1, 0								# t1 = index of first max element
+    li t2, 1								# t2 = current index
+    
+    beq a1, t6, loop_end					# array contains only one element, return 0
+    
+    mv t3, a0								# t3 = pointer to current element
+    addi t3, t3, 4
 loop_start:
     # TODO: Add your own implementation
+    lw t4, 0(t3)
+    ble t4, t0, loop_continue
+    mv t0, t4								# update max element
+    mv t1, t2								# update index of max element
+    
+loop_continue:
+	addi t2, t2, 1
+	addi t3, t3, 4
+    bne t2, a1, loop_start
+    
+loop_end:
+	mv a0, t1
+    jr ra
 
 handle_error:
     li a0, 36
