10c291ce46915a06e361e15271ceddd5a19d98d6
Eric Ruan <p76121071@gs.ncku.edu.tw>
2024-11-09 16:26:58 +0800

    Finish dot.s without using mulh instruction

--- src/dot.s
+++ src/dot.s
@@ -31,16 +31,83 @@ dot:
     blt a3, t0, error_terminate   
     blt a4, t0, error_terminate  
 
-    li t0, 0            
-    li t1, 0         
+    li t0, 0            					# t0 = dot product value
+    li t1, 0         						# t1 = element counter
 
+	addi sp, sp, -12
+	sw s0, 0(sp)
+	sw s1, 4(sp)
+	sw s2, 8(sp)
+	
+	mv t2, a0								# t2 = address of first array
+	mv t3, a1								# t3 = address of second array
+	slli a3, a3, 2
+	slli a4, a4, 2
 loop_start:
     bge t1, a2, loop_end
     # TODO: Add your own implementation
+    lw t4, 0(t2)							# t4 = value of element of t2
+    lw t5, 0(t3)							# t5 = value of element of t3
+    
+# =======================================
+multiply:
+	li t6, 0 								# t6 = t4 * t5
+	li s0, 1
+	xor s1, t4, t5							
+	srai s1, s1, 31							
+	
+	bltz t4, neg_a
+pos_a:
+	bltz t5, neg_b
+pos_b:
 
+loop:
+	and s2, t5, s0
+	beqz s2, skip_add
+	add t6, t6, t4
+	
+skip_add:
+	slli t4, t4, 1
+	srli t5, t5, 1
+	bnez t5, loop
+	
+	beqz s1, end
+	sub t6, x0, t6
+	j end
+	
+neg_a:
+	sub t4, x0, t4
+	j pos_a
+neg_b:
+	sub t5, x0, t5
+	j pos_b
+	
+end:
+	
+# =======================================
+    
+	
+    add t0, t0, t6
+    
+    
+loop_continue:
+	addi t1, t1, 1
+	add t2, t2, a3
+	add t3, t3, a4
+	j loop_start
+	
 loop_end:
     mv a0, t0
+    
+    lw s0, 0(sp)
+    lw s1, 4(sp)
+    lw s2, 8(sp)
+    addi sp, sp, 12
+    
     jr ra
+    
+    
+
 
 error_terminate:
     blt a2, t0, set_error_36
